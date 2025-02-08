ca91de3b114e03d0d3cf70f4f6e2b233bc4c3244
Aleksandar Obreshkov <aleksandar.obreshkov1@gmail.com>
2025-02-08 19:52:33 +0200

    Handle opening files from other repos

--- src/pluginConfigFileReader.ts
+++ src/pluginConfigFileReader.ts
@@ -1,17 +1,27 @@
+import * as fs from 'fs';
+import * as vscode from 'vscode';
+
 let CONCOURSE_CONFIG: any = null;
 
 export default function getConfiguration(): Map<string, string> {
 
-    if (CONCOURSE_CONFIG == null) {
-        CONCOURSE_CONFIG = readConfigFile()
-    }
+    // TODO: This could be converted to a singleton later on
+    // if (CONCOURSE_CONFIG == null) {
+    //     CONCOURSE_CONFIG = readConfigFile()
+    // }
 
-    return CONCOURSE_CONFIG
+    // return CONCOURSE_CONFIG
+    return readConfigFile()
 }
 
 
 function readConfigFile(): Map<string, string> {
-    let configMap = new Map<string, string>()
-    configMap.set("source-code", "C:\\Users\\aleks\\Projects\\pipeline")
-    return configMap
+    if (vscode.workspace.workspaceFolders == null ) {
+        return new Map()
+    }
+    let currentWorkPath = vscode.workspace.workspaceFolders[0].uri.fsPath
+    let configFileContent = fs.readFileSync(`${currentWorkPath}/concourser.json`, "utf-8")
+    console.log(configFileContent)
+    let config = JSON.parse(configFileContent)
+    return new Map(Object.entries(config))
 }
